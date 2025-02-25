9b1445c6e589ed17fa562569003de6a53d681036
jamerulhassan <jamerulhassan2k04@gmail.com>
2025-02-25 07:19:27 +0530

    Time: 51 ms (7.34%), Space: 45.3 MB (9.71%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -3,9 +3,11 @@ class Solution {
         int[] arr=new int[2];
         for(int i=0;i < nums.length;i++){
             for(int j=i+1;j < nums.length;j++){
-                if(nums[i]+nums[j] == target){
-                    arr[0]=i;
-                    arr[1]=j;
+                if(i != j){
+                    if(nums[i]+nums[j] == target){
+                        arr[0]=i;
+                        arr[1]=j;
+                    }
                 }
             }
         }
