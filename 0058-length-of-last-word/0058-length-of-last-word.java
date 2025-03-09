d729f3dad9da4b98e48177c555ab7b6952f1d66f
jamerulhassan <jamerulhassan2k04@gmail.com>
2025-03-09 15:16:10 +0530

    Time: 1 ms (33.33%), Space: 41.7 MB (65.14%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -1,6 +1,7 @@
 class Solution {
     public int lengthOfLastWord(String s) {
         String[] arr = s.split(" ");
-        return arr[arr.length -1].length();
+        int index = arr[arr.length -1].length();
+        return index;
     }
 }
