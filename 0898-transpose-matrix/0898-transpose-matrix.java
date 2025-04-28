f1fcc45a78dc8d8c897e24fc45979d92a77ba032
jamerulhassan <jamerulhassan2k04@gmail.com>
2025-04-28 12:16:50 +0530

    Time: 0 ms (100%), Space: 44.8 MB (87.13%) - LeetHub

---
+++ 0898-transpose-matrix/0898-transpose-matrix.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[][] transpose(int[][] matrix) {
+        return getArray(matrix);
+    }
+    static int[][] getArray(int[][] arr){
+        int[][] res = new int[arr[0].length][arr.length];
+        for(int i = 0 ; i < arr[0].length ; i++){
+            for(int j = 0;j < arr.length;j++){
+                res[i][j] = arr[j][i];
+            }
+        }
+        return res;
+    }
+}
