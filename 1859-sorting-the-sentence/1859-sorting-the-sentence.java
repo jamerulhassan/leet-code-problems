6a6cafa2c74289dcc00053c69c4c2c4b6b281d89
jamerulhassan <jamerulhassan2k04@gmail.com>
2025-05-08 10:22:31 +0530

    Time: 5 ms (6.23%), Space: 41.5 MB (75.49%) - LeetHub

--- 1859-sorting-the-sentence/1859-sorting-the-sentence.java
+++ 1859-sorting-the-sentence/1859-sorting-the-sentence.java
@@ -5,7 +5,7 @@ class Solution {
         String res = "";
         for(int i = 1;i <= arr.length;i++){
             for(int j = 0; j < arr.length;j++){
-                int last = Integer.parseInt(arr[j].substring(arr[j].length()-1,arr[j].length()));
+                int last = Character.getNumericValue( (arr[j].charAt(arr[j].length()-1)) );
                 if(i == last && i < arr.length){
                     res = res + arr[j].substring(0,arr[j].length()-1) + " ";
                 }
