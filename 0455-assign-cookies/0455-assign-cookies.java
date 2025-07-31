80e14d756d52f7592f77551184e8e974b39f88bc
jamerulhassan <jamerulhassan2k04@gmail.com>
2025-07-31 07:45:01 +0530

    Time: 139 ms (35.77%), Space: 48.5 MB (74.57%) - LeetHub

---
+++ 0455-assign-cookies/0455-assign-cookies.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+        if(s.length == 0 || g[0] > s[s.length-1]) return 0;
+        int index = 0, count = 0;
+        Boolean isObtained = false;
+        for(int i = 0 ; i < g.length; i++){
+            int greed = g[i];
+            for(int j = index; j < s.length;j++){
+                System.out.println(index);
+                if(greed <= s[j]){
+                    count++;
+                    index = j + 1;
+                    break;
+                }
+            }
+        }
+        return count;
+    }
+}
